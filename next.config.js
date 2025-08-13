79be46132f9c29743b64faf9403e43bb2bec0e44
saitamanzo <fujiwara_akira@yahoo.co.jp>
2025-08-13 22:46:19 +0900

    安定バージョン v01.00.03

---
+++ next.config.js
@@ -0,0 +1,12 @@
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	images: {
+		domains: [
+			'ahxgjgorxyteneqacrws.supabase.co',
+			// 他に必要なドメインがあれば追加
+		],
+	},
+};
+
+module.exports = nextConfig;
